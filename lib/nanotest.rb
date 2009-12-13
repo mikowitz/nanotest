dee0b1a5e902767f5855dac73fa60dbf42910a69
Michael Berkowitz <michael.berkowitz@gmail.com>
2009-12-13 10:55:04 -0500

    Adds error rescuing to Nanotest.assert

--- lib/nanotest.rb
+++ lib/nanotest.rb
@@ -3,15 +3,21 @@ module Nanotest
 
   @@failures, @@dots = [], []
 
-  def assert(msg=nil, file=nil, line=nil, &block)
+  def assert(msg="assertion failed", file=nil, line=nil, &block)
     unless block.call
       file ||= caller.first.split(':')[0]
       line ||= caller.first.split(':')[1]
-      @@failures << "(%s:%0.3d) %s" % [file, line, msg || "assertion failed"]
-      @@dots << 'F'
+      add_failure(file, line, msg)
     else
       @@dots << '.'
     end
+  rescue Exception => e
+    add_failure(file, line, e.message)
+  end
+
+  def self.add_failure(file, line, message) #:nodoc:
+    @@failures << "(%s:%0.3d) %s" % [file, line, message]
+    @@dots << 'F'
   end
 
   def self.results #:nodoc:
